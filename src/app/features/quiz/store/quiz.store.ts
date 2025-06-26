9ee9fb616167f51eb8c4fcc33a7ddd25215716b6
Valeriy Semenenko <gearmobile@gmail.com>
2025-06-26 23:10:06 +0300

    use store in another store

--- src/app/features/quiz/store/quiz.store.ts
+++ src/app/features/quiz/store/quiz.store.ts
@@ -8,13 +8,19 @@ import {
   withState,
 } from '@ngrx/signals';
 import { initialQuizSlice, QuizSlice } from './quiz.slice';
-import { computed, effect } from '@angular/core';
+import { computed, effect, inject } from '@angular/core';
 import { addAnswer, resetQuiz } from './quiz.updaters';
 import { getCorrectCount } from './quiz.helpers';
+import { AppStore } from '../../../store/app.store';
+import { translate, translateToPairs } from '../../../store/app.helper';
+import { QUESTION_CAPTION } from '../../../data/dictionaries';
 
 export const QuizStore = signalStore(
   withState(initialQuizSlice),
   withComputed((store) => {
+    const appStore = inject(AppStore);
+    const selectedDictionary = appStore.selectedDictionary;
+
     const currentQuestionIndex = computed(() => store.answers().length);
     const isDone = computed(
       () => store.answers().length === store.questions().length
@@ -27,12 +33,25 @@ export const QuizStore = signalStore(
       getCorrectCount(store.answers(), store.questions())
     );
 
+    const translatedTitle = computed(() =>
+      translate(QUESTION_CAPTION, selectedDictionary())
+    );
+    const captionColors = computed(() =>
+      translateToPairs(currentQuestion().caption, selectedDictionary())
+    );
+    const answerColors = computed(() =>
+      translateToPairs(currentQuestion().answers, selectedDictionary())
+    );
+
     return {
       currentQuestionIndex,
       isDone,
       currentQuestion,
       questionsCount,
       correctCount,
+      translatedTitle,
+      captionColors,
+      answerColors,
     };
   }),
   withMethods((store) => ({
--- src/app/store/app.store.ts
+++ src/app/store/app.store.ts
@@ -1,18 +1,29 @@
 import {
   patchState,
   signalStore,
+  withComputed,
   withHooks,
   withMethods,
   withState,
 } from '@ngrx/signals';
 import { initialState } from './app.slice';
-import { inject } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { DICTIONARIES_TOKEN } from '../tokens/dictionaries.token';
 import { changeLanguageUpdater } from './app.updater';
+import { getDictionary } from './app.helper';
 
 export const AppStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
+  withComputed((store) => {
+    const dictionaries = inject(DICTIONARIES_TOKEN);
+
+    return {
+      selectedDictionary: computed(() =>
+        getDictionary(store.selectedLanguage(), dictionaries)
+      ),
+    };
+  }),
   withMethods((store) => {
     const dictionaries = inject(DICTIONARIES_TOKEN);
     const languages = Object.keys(dictionaries);
